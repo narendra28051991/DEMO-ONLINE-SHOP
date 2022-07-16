ef3b6a849e81d9d65d389a5c79c00d6b99468ff0
narendra28051991 <narendra28051991@gmail.com>
2022-07-16 23:12:27 +0200

    demo online shop complete

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAdi2Bpo4_3GRwqSf22vbL_8pG3c5kEwBo",
+    authDomain: "fir-online-shop-e1117.firebaseapp.com",
+    projectId: "fir-online-shop-e1117",
+    storageBucket: "fir-online-shop-e1117.appspot.com",
+    messagingSenderId: "20522982824",
+    appId: "1:20522982824:web:9496bc7ac91a6099c4519d"
+  };
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
